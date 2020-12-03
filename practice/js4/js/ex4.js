f73cdd49bb763be48841101596f1822f266f0758
QuanKHs <quanphamluong@gmail.com>
2020-12-03 14:06:04 +0700

    commit js4

--- practice/js4/js/ex4.js
+++ practice/js4/js/ex4.js
@@ -9,40 +9,41 @@ const ChannelFee_Corp = 50;
 var typeFam = document.getElementById("typeFam");
 var typeCorp = document.getElementById("typeCorp");
 
-typeFam.addEventListener("click", function(){
-    document.getElementById("accounts").setAttribute("disabled", true);
+typeFam.addEventListener("click", function () {
+    document.getElementById("cable").setAttribute("disabled", true);
 })
 
-typeCorp.addEventListener("click", function(){
-    document.getElementById("accounts").removeAttribute("disabled");
+typeCorp.addEventListener("click", function () {
+    document.getElementById("cable").removeAttribute("disabled");
 })
 
-function calcCableBill(type,service, channel){
-    if(type === "family"){
-        return BillFee_Fam + ServiceFee_Fam + channel*ChannelFee_Fam;
-    }else if(type ==="corporate"){
-        if (service <= 10){
-            return BillFee_Corp + ServiceFee_Corp_1 + channel*ChannelFee_Corp; 
-        }else{
-            return BillFee_Corp + ServiceFee_Corp_1 + (service - 10)*ServiceFee_Corp_2 + channel*ChannelFee_Corp;
+function calcCableBill(type, service, channel) {
+    if (type === "family") {
+        return BillFee_Fam + ServiceFee_Fam + channel * ChannelFee_Fam;
+    } else if (type === "corporate") {
+        if (service <= 10) {
+            return BillFee_Corp + ServiceFee_Corp_1 + channel * ChannelFee_Corp;
+        } else {
+            return BillFee_Corp + ServiceFee_Corp_1 + (service - 10) * ServiceFee_Corp_2 + channel * ChannelFee_Corp;
         }
     }
 }
 
-function checkSubscriptionType(){
-    if(typeFam.checked){
+function checkSubscriptionType() {
+    if (typeFam.checked) {
         return "family";
-    }else{
+    } else {
         return "corporate";
     }
 }
 
-document.getElementById("calcCable").addEventListener("click", function(){
-    var account = parseFloat(document.getElementById("accounts").value);
+document.getElementById("calcCable").addEventListener("click", function () {
+    var account = document.getElementById("accounts").value;
+    var cable = parseFloat(document.getElementById("cable").value);
     var channels = parseFloat(document.getElementById("channels").value);
-    
+
     var type = checkSubscriptionType(typeFam, typeCorp);
-    var totalBill = calcCableBill(type, account, channels);
-    
-    document.getElementById("cable").innerHTML = "Customer has to pay " + totalBill + "$ for Cable Bill.";
+    var totalBill = calcCableBill(type, cable, channels);
+
+    document.getElementById("cableBill").innerHTML = "Customer " + account + " has to pay " + totalBill + "$ for Cable Bill.";
 })
